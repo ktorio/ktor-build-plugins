3dfb794585ab34aa0eabee00896dce50e4f86337
Rustam Musin <rustam.musin@jetbrains.com>
2022-04-26 11:17:42 +0300

    Drop println when plugin is loaded

--- plugin/src/main/kotlin/io/ktor/plugin/KtorPlugin.kt
+++ plugin/src/main/kotlin/io/ktor/plugin/KtorPlugin.kt
@@ -7,7 +7,6 @@ import org.gradle.api.Project
 @Suppress("unused") // Gradle Plugin is not used directly
 abstract class KtorPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        println("Ktor plugin is loaded")
         project.plugins.apply(ShadowPlugin::class.java)
         project.tasks.create("buildShadowJar") {
             it.dependsOn("shadowJar")
