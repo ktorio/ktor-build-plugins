e826b1e9a36383dc5a3ea00390285aec996d8a94
Rustam Musin <rustam.musin@jetbrains.com>
2022-06-15 23:55:57 +0200

    KTOR-4471 Add runFatJar task

--- plugin/src/main/kotlin/io/ktor/plugin/features/FatJar.kt
+++ plugin/src/main/kotlin/io/ktor/plugin/features/FatJar.kt
@@ -12,9 +12,14 @@ abstract class FatJarExtension {
     var archiveFileName: String? = null
 }
 
+const val FAT_JAR_EXTENSION_NAME = "fatJar"
+
 const val BUILD_FAT_JAR_TASK_NAME = "buildFatJar"
 private const val BUILD_FAT_JAR_TASK_DESCRIPTION = "Builds a combined JAR of project and runtime dependencies."
-const val FAT_JAR_EXTENSION_NAME = "fatJar"
+
+const val RUN_FAT_JAR_TASK_NAME = "runFatJar"
+private const val RUN_FAT_JAR_TASK_DESCRIPTION =
+    "Builds a combined JAR of project and runtime dependencies and runs it."
 
 private const val CONFIGURE_SHADOW_JAR_TASK_NAME = "configureShadowJar"
 
@@ -22,11 +27,10 @@ fun configureFatJar(project: Project) {
     project.plugins.apply(ShadowPlugin::class.java)
     val tasks = project.tasks
 
-    // We need to set `mainClassName` even if `mainClass` is set, because ShadowJar Plugin v6 needs it.
-    // We can remove this block when we move to ShadowJar Plugin v7 or above.
-    val shadowJar = tasks.withType(ShadowJar::class.java)
     val configureShadowJar = tasks.register(CONFIGURE_SHADOW_JAR_TASK_NAME) {
         it.doLast {
+            // We need to set `mainClassName` even if `mainClass` is set, because ShadowJar Plugin v6 needs it.
+            // We can remove this block when we move to ShadowJar Plugin v7 or above.
             if (project.findProperty("mainClassName") == null) {
                 val application = project.extensions.getByType(JavaApplication::class.java)
                 application.mainClass.orNull?.let { mainClassName ->
@@ -37,12 +41,21 @@ fun configureFatJar(project: Project) {
     }
 
     val fatJarExtension = project.createKtorExtension<FatJarExtension>(FAT_JAR_EXTENSION_NAME)
-    shadowJar.configureEach {
-        fatJarExtension.archiveFileName?.let { name -> it.archiveFileName.set(name) }
+    val shadowJar = tasks.named("shadowJar", ShadowJar::class.java) {
         it.dependsOn(configureShadowJar)
+        fatJarExtension.archiveFileName?.let { name -> it.archiveFileName.set(name) }
     }
 
-    tasks.registerKtorTask(BUILD_FAT_JAR_TASK_NAME, BUILD_FAT_JAR_TASK_DESCRIPTION) {
+    val buildFatJar = tasks.registerKtorTask(BUILD_FAT_JAR_TASK_NAME, BUILD_FAT_JAR_TASK_DESCRIPTION) {
         dependsOn(shadowJar)
+    }
+
+    tasks.registerKtorTask(RUN_FAT_JAR_TASK_NAME, RUN_FAT_JAR_TASK_DESCRIPTION) {
+        dependsOn(buildFatJar)
+        doLast {
+            project.javaexec { java ->
+                java.classpath(shadowJar.flatMap { it.archiveFile })
+            }
+        }
     }
 }
