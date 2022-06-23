6f185692c28284c6ece641f6185cb3bdd2836a93
Rustam Musin <rustam.musin@jetbrains.com>
2022-06-23 16:20:36 +0200

    KTOR-4470 Mark unsupported Shadow tasks as not supported

--- plugin/src/main/kotlin/io/ktor/plugin/features/FatJar.kt
+++ plugin/src/main/kotlin/io/ktor/plugin/features/FatJar.kt
@@ -5,7 +5,9 @@ import com.github.jengelman.gradle.plugins.shadow.ShadowJavaPlugin
 import com.github.jengelman.gradle.plugins.shadow.ShadowPlugin
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.api.Project
+import org.gradle.api.Task
 import org.gradle.api.plugins.JavaApplication
+import org.gradle.api.tasks.TaskContainer
 
 abstract class FatJarExtension(project: Project) {
     /**
@@ -32,9 +34,32 @@ private fun configureMainClass(project: Project) {
     application.mainClass.orNull?.let { mainClassName -> application.mainClassName = mainClassName }
 }
 
+@Suppress("UnstableApiUsage")
+private fun Task.shadowTaskIsNotCompatibleWithConfigurationCache(taskName: String) {
+    notCompatibleWithConfigurationCache(
+        /* reason = */ "`$taskName` is not compatible yet: " +
+                "https://github.com/johnrengelman/shadow/issues/775"
+    )
+}
+
+private val INCOMPATIBLE_SHADOW_TASK_NAMES = arrayOf(
+    ShadowApplicationPlugin.getSHADOW_INSTALL_TASK_NAME(),
+    ShadowApplicationPlugin.getSHADOW_RUN_TASK_NAME()
+)
+
+private fun markShadowTasksAsNotCompatibleWithConfigurationCache(tasks: TaskContainer) {
+    INCOMPATIBLE_SHADOW_TASK_NAMES.forEach { taskName ->
+        tasks.named(taskName) {
+            it.shadowTaskIsNotCompatibleWithConfigurationCache(taskName)
+        }
+    }
+}
+
 fun configureFatJar(project: Project) {
     project.plugins.apply(ShadowPlugin::class.java)
     val tasks = project.tasks
+
+    markShadowTasksAsNotCompatibleWithConfigurationCache(tasks)
 
     val fatJarExtension = project.createKtorExtension<FatJarExtension>(FAT_JAR_EXTENSION_NAME)
     val shadowJar = tasks.named(ShadowJavaPlugin.getSHADOW_JAR_TASK_NAME(), ShadowJar::class.java) {
