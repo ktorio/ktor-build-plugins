f449c3e9cc3af200980d3d4fe7ff57a66d090118
Rustam Musin <rustam.musin@jetbrains.com>
2022-06-30 13:42:38 +0200

    KTOR-4596 Disable native image sample

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,4 +5,4 @@ pluginManagement {
 
 include("samples:ktor-fatjar-sample")
 include("samples:ktor-docker-sample")
-include("samples:ktor-native-image-sample")
+//include("samples:ktor-native-image-sample") // KTOR-4596 Disable Native image related tasks
