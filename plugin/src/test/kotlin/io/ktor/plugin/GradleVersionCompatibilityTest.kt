66fce2e0004b1039baca68f02467d9a967db9b41
Rustam Musin <jvmusin@gmail.com>
2022-09-29 13:29:23 +0200

    KTOR-4775 Mark task's GCC incompatibility only with Gradle v7.4+ (#51)
    This will allow using the plugin in projects with Gradle version lower than v7.4 without runtime exceptions like MethodNotFoundException

--- plugin/src/test/kotlin/io/ktor/plugin/GradleVersionCompatibilityTest.kt
+++ plugin/src/test/kotlin/io/ktor/plugin/GradleVersionCompatibilityTest.kt
@@ -4,24 +4,40 @@ import org.junit.jupiter.api.io.TempDir
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import java.io.File
+import kotlin.test.Test
 
 class GradleVersionCompatibilityTest {
-    private fun buildProject(gradleVersion: String, projectDir: File, expectSuccess: Boolean) {
+    @ParameterizedTest
+    @ValueSource(strings = ["6.7", "7.3.3", "7.5.1"])
+    fun testProjectBuild(gradleVersion: String, @TempDir projectDir: File) {
         projectDir.resolve("build.gradle").writeText("plugins { id 'io.ktor.plugin' }")
         createGradleRunner(projectDir)
             .withGradleVersion(gradleVersion)
-            .run { if (expectSuccess) build() else buildAndFail() }
+            .build()
     }
 
-    @ParameterizedTest
-    @ValueSource(
-        strings = [
-            "6.7",
-//            "6.7.1", "6.8", "6.8.1", "6.8.2", "6.8.3", "6.9", "6.9.1", "6.9.2",
-//            "7.0", "7.0.1", "7.0.2", "7.1", "7.1.1", "7.2", "7.3", "7.3.1", "7.3.2", "7.3.3", "7.4", "7.4.1",
-            "7.4.2"
-        ]
-    )
-    fun testProjectBuild(gradleVersion: String, @TempDir projectDir: File) =
-        buildProject(gradleVersion, projectDir, true)
+    @Test
+    fun `test not fails when gradle is old and task is not GCC compatible`(@TempDir projectDir: File) {
+        val gradleVersion = "7.3.3" // any version lower than 7.4
+
+        // any incompatible with Gradle Configuration Cache task, see https://github.com/johnrengelman/shadow/issues/775
+        val incompatibleTask = "runShadow"
+
+        buildProject(
+            projectDir = projectDir,
+            buildGradleKtsContent = """
+                plugins {
+                    kotlin("jvm") version "1.7.0"
+                    id("io.ktor.plugin")
+                }
+                repositories.mavenCentral()
+                application.mainClass.set("my.org.MainKt")
+            """.trimIndent(),
+            settingsGradleKtsContent = "",
+            mainKtContent = "package my.org\nfun main() {}",
+            buildCommand = incompatibleTask,
+            expectSuccess = true,
+            gradleVersion = gradleVersion
+        )
+    }
 }
