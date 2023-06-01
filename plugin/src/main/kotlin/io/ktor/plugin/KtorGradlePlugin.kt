32a983c4a6bfb0a96f00433699209f92c88d6984
Aleksei Tirman <xrenas@gmail.com>
2023-06-01 15:47:52 +0300

    KTOR-5958 Update Ktor to 2.3.1 (#87)

--- plugin/src/main/kotlin/io/ktor/plugin/KtorGradlePlugin.kt
+++ plugin/src/main/kotlin/io/ktor/plugin/KtorGradlePlugin.kt
@@ -5,7 +5,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.ApplicationPlugin
 
-const val KTOR_VERSION = "2.3.0"
+const val KTOR_VERSION = "2.3.1"
 
 @Suppress("unused") // Gradle Plugin is not used directly
 abstract class KtorGradlePlugin : Plugin<Project> {
