de899d8fbef86f95ccf959179d244a259679c24e
leonid.stashevsky <leonid.stashevsky@gmail.com>
2024-02-29 10:15:20 +0100

    Fix build after version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
-import org.gradle.api.tasks.testing.logging.TestLogEvent
-
 plugins {
-    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.jvm) apply false
 }
 
 allprojects {
@@ -10,15 +8,3 @@ allprojects {
     }
 }
 
-subprojects {
-    tasks.withType<Test> {
-        useJUnitPlatform()
-        testLogging.events(*TestLogEvent.values())
-    }
-    apply(plugin = rootProject.libs.plugins.kotlin.jvm.get().pluginId)
-    dependencies {
-        implementation(rootProject.libs.ktor.server.core)
-        implementation(rootProject.libs.ktor.server.cio)
-        implementation(rootProject.libs.logback)
-    }
-}
