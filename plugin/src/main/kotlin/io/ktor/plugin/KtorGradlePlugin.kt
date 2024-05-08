874aaaa5a6732ffb7d5d188f1eeb20323e95db7f
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2024-05-08 16:29:05 +0300

    Release for Ktor 2.3.11

--- plugin/src/main/kotlin/io/ktor/plugin/KtorGradlePlugin.kt
+++ plugin/src/main/kotlin/io/ktor/plugin/KtorGradlePlugin.kt
@@ -5,7 +5,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.ApplicationPlugin
 
-const val KTOR_VERSION = "2.3.10"
+const val KTOR_VERSION = "2.3.11"
 
 @Suppress("unused") // Gradle Plugin is not used directly
 abstract class KtorGradlePlugin : Plugin<Project> {
