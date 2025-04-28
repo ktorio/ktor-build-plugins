b898c50e309f6968912d36fe7f982ea65d6fdb00
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2025-04-28 16:45:16 +0200

    Fix BOM configuring for KMP projects

--- plugin/src/main/kotlin/io/ktor/plugin/features/BomFile.kt
+++ plugin/src/main/kotlin/io/ktor/plugin/features/BomFile.kt
@@ -1,10 +1,26 @@
 package io.ktor.plugin.features
 
 import io.ktor.plugin.*
+import io.ktor.plugin.internal.*
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
 
-fun configureBomFile(project: Project) {
-    with(project.dependencies) {
-        add("implementation", platform("io.ktor:ktor-bom:$KTOR_VERSION"))
+fun configureBomFile(project: Project) = with(project) {
+    whenKotlinJvmApplied {
+        dependencies.add("implementation", dependencies.ktorBom)
+    }
+
+    whenKotlinMultiplatformApplied {
+        with(kotlinExtension as KotlinMultiplatformExtension) {
+            sourceSets.commonMain.dependencies {
+                implementation(dependencies.ktorBom)
+            }
+        }
     }
 }
+
+private val DependencyHandler.ktorBom: Dependency
+    get() = platform("io.ktor:ktor-bom:$KTOR_VERSION")
