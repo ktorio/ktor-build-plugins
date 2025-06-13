a99b7600f00820b423ed205cf22662a63d56f839
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2025-06-13 13:17:14 +0300

    Release 3.2.0 (#201)

--- plugin/src/main/kotlin/io/ktor/plugin/KtorGradlePlugin.kt
+++ plugin/src/main/kotlin/io/ktor/plugin/KtorGradlePlugin.kt
@@ -57,7 +57,7 @@ public abstract class KtorGradlePlugin : Plugin<Project> {
 
     public companion object {
         /** The Ktor plugin version. Usually it is equal to the Ktor version used in a project. */
-        public const val VERSION: String = "3.1.3"
+        public const val VERSION: String = "3.2.0"
 
         /** The group name used for Ktor tasks. */
         public const val TASK_GROUP: String = "Ktor"
