29bfa79413f7f6f69c985c9aef3fd13f172463c7
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2025-07-28 12:26:51 +0200

    KTOR-8316 Update OpenApiSample

--- samples/ktor-openapi-sample/src/main/kotlin/Application.kt
+++ samples/ktor-openapi-sample/src/main/kotlin/Application.kt
@@ -2,43 +2,127 @@ package io.ktor.samples.openapi
 
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.install
 import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
-import io.ktor.server.response.respond
-import io.ktor.server.response.respondText
-import io.ktor.server.routing.get
-import io.ktor.server.routing.post
-import io.ktor.server.routing.routing
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.plugins.openapi.openAPI
+import io.ktor.server.request.receive
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.util.toMap
 import kotlinx.serialization.Serializable
 
 fun main() {
     embeddedServer(CIO, port = 8080) {
+        install(ContentNegotiation) {
+            json()
+        }
+
         routing {
             // Main page for marketing
             get("/") {
                 call.respondText("<html><body><h1>Hello, World</h1></body></html>", ContentType.Text.Html)
             }
 
-            /*
-             * Save a message.
-             *
-             * @body [Message] the message to save
+            /**
+             * API endpoints for users.
              */
-            post("/messages") {
-                call.respond(HttpStatusCode.Created)
-            }
+            userCrud(ListRepository())
 
             /**
-             * Get a list of messages.
-             *
-             * @response 200 [List<Message>] A list of messages
+             * Get the OpenAPI specification.
              */
-            get("/messages") {
-                call.respond(listOf(Message("Hello, world!")))
-            }
+            openAPI("/docs", swaggerFile = "api.json")
         }
     }.start(wait = true)
 }
 
+fun Routing.userCrud(repository: Repository<User>) {
+    route("/api") {
+
+        route("/users") {
+
+            /**
+             * Get a list of users.
+             *
+             * @response 200 A list of users.
+             */
+            get {
+                val query = call.request.queryParameters.toMap()
+                val list = repository.list(query)
+                call.respond(list)
+            }
+
+            /**
+             * Get a single user
+             *
+             * @param id The ID of the user
+             * @response 404 The user was not found
+             * @response 200 [User] The user.
+             */
+            get("{id}") {
+                val user = repository.get(call.parameters["id"]!!)
+                    ?: return@get call.respond(HttpStatusCode.NotFound)
+                call.respond(user)
+            }
+
+            /**
+             * Save a new user.
+             *
+             * @body [User] the user to save.
+             */
+            post {
+                repository.save(call.receive())
+                call.respond(HttpStatusCode.Created)
+            }
+
+            /**
+             * Delete a user.
+             * @param id The ID of the user
+             * @response 204 The user was deleted
+             */
+            delete("{id}") {
+                repository.delete(call.parameters["id"]!!)
+                call.respond(HttpStatusCode.NoContent)
+            }
+        }
+    }
+}
+
+
+
+interface Repository<E> {
+    fun get(id: String): E?
+    fun save(entity: E)
+    fun delete(id: String)
+    fun list(query: Map<String, List<String>>): List<E>
+}
+
+interface Entity {
+    val id: String
+}
+
+class ListRepository<E: Entity>: Repository<E> {
+    private val list: MutableList<E> = mutableListOf()
+
+    override fun list(query: Map<String, List<String>>): List<E> {
+        return list.toList()
+    }
+
+    override fun get(id: String): E? {
+        return list.find { it.id == id }
+    }
+
+    override fun save(entity: E) {
+        list.add(entity)
+    }
+
+    override fun delete(id: String) {
+        list.removeIf { it.id == id }
+    }
+}
+
 @Serializable
-data class Message(val message: String)
+data class User(override val id: String, val name: String): Entity
