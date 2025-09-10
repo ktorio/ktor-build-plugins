b6d46ea9837639233857d52e84727b02b9f0d5fa
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2025-09-10 18:04:37 +0200

    Add module build-logic

---
+++ build-logic/settings.gradle.kts
@@ -0,0 +1,18 @@
+@file:Suppress("UnstableApiUsage")
+
+dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+
+rootProject.name = "build-logic"
