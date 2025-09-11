663e3315d08089ff36c750033c212e3429bc892c
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2025-09-11 10:27:21 +0200

    Update Gradle to v9.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@ The format is based on Keep a Changelog: https://keepachangelog.com/en/1.1.0/
 
 ## [Unreleased]
 
-*No changes yet*
+- Update Gradle to 9.0.0
 
 ## [3.2.3] - 2025-07-29
 
--- plugin/src/test/kotlin/io/ktor/plugin/GradleVersionCompatibilityTest.kt
+++ plugin/src/test/kotlin/io/ktor/plugin/GradleVersionCompatibilityTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.provider.ValueSource
 
 class GradleVersionCompatibilityTest : IntegrationTest() {
     @ParameterizedTest
-    @ValueSource(strings = ["8.3", "8.14"])
+    @ValueSource(strings = ["8.3", "8.14", "9.0.0"])
     fun testProjectBuild(gradleVersion: String) {
         runBuild { withGradleVersion(gradleVersion) }
     }
