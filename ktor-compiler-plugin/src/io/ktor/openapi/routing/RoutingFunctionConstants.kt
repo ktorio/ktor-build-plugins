e788f7a39aed6085000eea6ee5f58ebc88610fc3
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2026-01-28 17:08:05 +0100

    KTOR-9291 More general support for routing functions

--- ktor-compiler-plugin/src/io/ktor/openapi/routing/RoutingFunctionConstants.kt
+++ ktor-compiler-plugin/src/io/ktor/openapi/routing/RoutingFunctionConstants.kt
@@ -2,6 +2,10 @@ package io.ktor.openapi.routing
 
 object RoutingFunctionConstants {
     const val ROUTING_PACKAGE = "io.ktor.server.routing"
+    const val ROUTE_INTERFACE = "io.ktor.server.routing.Route"
+    const val ROUTING_CONTEXT = "io.ktor.server.routing.RoutingContext"
+
+
     const val ROUTE = "route"
     const val GET = "get"
     const val POST = "post"
@@ -10,7 +14,7 @@ object RoutingFunctionConstants {
     const val HEAD = "head"
     const val OPTIONS = "options"
     const val PATCH = "patch"
+    const val QUERY = "query"
 
-    val HTTP_METHODS = setOf(GET, POST, PUT, DELETE, HEAD, OPTIONS, PATCH)
-    val ROUTING_FUNCTION_NAMES = HTTP_METHODS + ROUTE
+    val HTTP_METHODS = setOf(GET, POST, PUT, DELETE, HEAD, OPTIONS, PATCH, QUERY)
 }
