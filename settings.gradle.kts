5b1693af4b429ba4d90f2af979b5cbb198b1fe71
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2026-02-09 14:28:00 +0100

    Update one more Ktor EAP repository URL (#274)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ dependencyResolutionManagement {
     repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
     repositories {
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
+        maven("https://redirector.kotlinlang.org/maven/ktor-eap")
     }
 }
 
