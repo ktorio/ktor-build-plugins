ffd9a9b49cd3b662906e51dd8243fab4b55d835b
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2026-03-24 13:26:23 +0100

    KTOR-9422 Support delegate property access inference

--- ktor-compiler-plugin/testData/openapi/Parameters.kt
+++ ktor-compiler-plugin/testData/openapi/Parameters.kt
@@ -6,6 +6,8 @@ import io.ktor.server.application.Application
 import io.ktor.server.request.header
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.util.getOrFail
+import io.ktor.server.util.getValue
 
 fun Application.installParameters() {
     routing {
@@ -19,10 +21,20 @@ fun Application.installParameters() {
                 call.request.pathVariables["c"],
                 call.queryParameters["d"],
                 call.request.queryParameters["e"],
-                call.request.headers["f"],
-                call.request.headers.getAll("g"),
-                call.request.header("h")
+                call.request.queryParameters.getAll("f"),
+                call.request.queryParameters.getOrFail("g"),
+                call.request.headers["h"],
+                call.request.headers.getAll("i"),
+                call.request.header("j")
             ).joinToString())
+        }
+
+        get("/parameters/delegates/{a}/{b}") {
+            val a: String by call.parameters
+            val b: Int by call.pathParameters
+            val c: String by call.queryParameters
+
+            call.respondText(listOf(a, b, c).joinToString())
         }
     }
 }
