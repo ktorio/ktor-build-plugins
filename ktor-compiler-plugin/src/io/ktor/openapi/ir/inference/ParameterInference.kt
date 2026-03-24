4c119e45f7e9f7308096ce4fba5f69c3875947ab
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2026-03-24 13:26:23 +0100

    KTOR-9422 Support delegate property access inference

--- ktor-compiler-plugin/src/io/ktor/openapi/ir/inference/ParameterInference.kt
+++ ktor-compiler-plugin/src/io/ktor/openapi/ir/inference/ParameterInference.kt
@@ -2,36 +2,72 @@ package io.ktor.openapi.ir.inference
 
 import io.ktor.openapi.ir.*
 import io.ktor.openapi.routing.*
+import io.ktor.openapi.routing.TypeReference.Companion.asReference
 import org.jetbrains.kotlin.ir.declarations.IrParameterKind
 import org.jetbrains.kotlin.ir.expressions.IrCall
+import org.jetbrains.kotlin.ir.expressions.IrLocalDelegatedPropertyReference
 import org.jetbrains.kotlin.ir.types.classFqName
 import org.jetbrains.kotlin.ir.types.isString
 
 val ParameterInference = IrCallHandlerInference { call: IrCall ->
-    if (!call.symbol.owner.name.asString().startsWith("get")) return@IrCallHandlerInference null
-    val receiver = call.functionReceiver ?: return@IrCallHandlerInference null
-    if (receiver.type.classFqName?.asString() != "io.ktor.util.StringValues") return@IrCallHandlerInference null
-    val getParameter = call.symbol.owner.parameters.firstOrNull {
-        it.kind == IrParameterKind.Regular && it.type.isString()
-    } ?: return@IrCallHandlerInference null
+    val functionName = call.symbol.owner.name.asString()
 
-    val key = call.arguments[getParameter.indexInParameters]
-        ?.let { LocalReference.of(it) }
-        ?: return@IrCallHandlerInference null
+    when (functionName) {
+        // StringValues lookup
+        "get", "getAll", "getOrFail" -> {
+            val keyArg = call.symbol.owner.parameters.firstOrNull {
+                it.kind == IrParameterKind.Regular && it.type.isString()
+            } ?: return@IrCallHandlerInference null
+            val parameterName = call.arguments[keyArg.indexInParameters]
+                ?.let { LocalReference.of(it) }
+                ?: return@IrCallHandlerInference null
+            inferFromStringValuesAccess(
+                call = call,
+                parameterName = parameterName,
+                parameterType = null
+            )
+        }
+        // Delegate property access
+        "getValue" -> {
+            val receiver = call.functionReceiver ?: return@IrCallHandlerInference null
+            if (receiver.type.classFqName?.asString() != "io.ktor.http.Parameters")
+                return@IrCallHandlerInference null
+            val propertyReference = call.arguments.filterIsInstance<IrLocalDelegatedPropertyReference>().firstOrNull()
+                ?: return@IrCallHandlerInference null
+            val propertyName = propertyReference.symbol.owner.name.asString()
+            inferFromStringValuesAccess(
+                call = call,
+                parameterName = LocalReference.of(propertyName),
+                parameterType = call.type.asReference()
+            )
+        }
 
-    val receiverArgCall = call.arguments[receiver.indexInParameters] as? IrCall
-
-    // we can sometimes infer what kind of parameter it is from the receiver
-    // this is not the case for the usual `call.parameters` reference,
-    // or when we're not using a property getter
-    when(receiverArgCall?.symbol?.owner?.name?.asString()) {
-        "<get-headers>" -> listOf(RouteField.Parameter(ParamIn.HEADER, key))
-        "<get-pathVariables>",
-        "<get-pathParameters>" -> listOf(RouteField.Parameter(ParamIn.PATH, key))
-        "<get-queryParameters>" -> listOf(RouteField.Parameter(ParamIn.QUERY, key))
-
-        // ambiguous scenario, we avoid defining `in` for now
-        // this can usually be inferred later at runtime
-        else -> listOf(RouteField.Parameter(name = key))
+        else -> null
     }
+}
+
+private fun inferFromStringValuesAccess(
+    call: IrCall,
+    parameterName: LocalReference,
+    parameterType: TypeReference?,
+): List<RouteField>? {
+    val receiver = call.functionReceiver ?: return null
+    val receiverTypeName = receiver.type.classFqName?.asString()
+    if (receiverTypeName !in listOf("io.ktor.util.StringValues", "io.ktor.http.Parameters")) return null
+
+    val receiverCall = call.arguments[receiver.indexInParameters] as? IrCall
+    val inValue = when (receiverCall?.symbol?.owner?.name?.asString()) {
+        "<get-headers>" -> ParamIn.HEADER
+        "<get-pathVariables>",
+        "<get-pathParameters>" -> ParamIn.PATH
+        "<get-queryParameters>" -> ParamIn.QUERY
+        else -> null
+    }
+    return listOf(
+        RouteField.Parameter(
+            `in` = inValue,
+            name = parameterName,
+            typeReference = parameterType
+        )
+    )
 }
